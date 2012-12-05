9b97063a583ae37f4b00969d77ac52d56135dc07
kirk@fyrebse.com <kirk@wkdthemes.com>
2012-12-05 18:18:48 +1000

    Added example of setting up requirements backend

--- _config.php
+++ _config.php
@@ -1,2 +1,6 @@
 <?php
-	Requirements::set_backend(new WkdRequirementsBackendExtension());
+	// The following is an example of setting up a new
+	// requirements backend
+
+	//Requirements::set_backend(new WkdRequirementsBackendExtension());
+	//Requirements::set_write_js_to_body(false);
